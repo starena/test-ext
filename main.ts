28fae0f5ade16b5ef66c8466f125d66db4a34a2c
Starena <stef@ziza.be>
2021-12-23 22:44:24 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
 namespace myTest {
-    export function plotA (index: number) : void {
+    /**
+     * My bla bla
+     */
+    //% blockId=mytestplotat
+    //% block=" plot at index"
+    //% index.min=5 index.max=25
+    export function plotAt (index: number) : void {
         index |= 0 
         const y = Math.floor(index / 5);
         const x = Math.floor(index % 5);
